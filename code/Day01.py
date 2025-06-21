7cbb097450d1d3f99b4b3a128a3a7230b5da94ef
Avantikaj <avantikajunagade2019@email.com>
2025-06-21 21:59:39 +0530

    what's learned today! :)

--- code/Day01.py
+++ code/Day01.py
@@ -1,5 +1,57 @@
 # this is my first day of coding pratice of Python and Git understanding
-
+print("Let's start with Hello World!")
 print("Entering the Coding World")
-
 print("Trying to understand things")
+
+#Let's start with basics 
+#Variables 
+
+name="Avantika"
+age=22
+favroite_no= 12.69
+
+age= 18 #value changing
+Age= 45 #case Sensitive
+
+print(name)
+print(age)
+print(favroite_no)
+print(Age)
+
+#Data Types
+
+count=120
+print(type(count))
+print(count + 40)
+
+temprature=35.27
+print(type(temprature))
+print(256*temprature)
+
+names="Junagade"
+print(type(names))
+print(name +" "+ names)
+
+is_it=True
+print(type(is_it))
+print(not is_it)
+
+#datatype conversion
+my_str ="100"
+int_str=int(my_str)
+print(int_str + 20)
+
+a= 10
+b=20 
+a,b= b,a #multiple assigment
+print(a,b)
+
+#Taking user input
+
+Names=input("Enter Your Name:")
+print("Hello " +Names)
+
+num1 =input("Enter the first number:")
+num2 = input("Enter the second number: ")
+sum = int(num1) + int(num2)
+print("The addition of numbers is :" , sum)
